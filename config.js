8f4af9075e8af659d1315be0682518247b96b3a2
Phạm Quốc Thanh <110448841+ShayNeeo@users.noreply.github.com>
2025-04-16 21:15:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gửi người mình thương",
-    introTitle: 'Huỳnh Thương',
+    introTitle: 'Đan Thanh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
